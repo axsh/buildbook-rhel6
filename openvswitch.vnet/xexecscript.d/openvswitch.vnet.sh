e19937edeb167bcf14f105696cf7f0dd1fc5d8d1
rakshasa <sundell.software@gmail.com>
2016-12-08 19:01:56 +0900

    Use new dropbox links for openvswitch.

--- openvswitch.vnet/xexecscript.d/openvswitch.vnet.sh
+++ openvswitch.vnet/xexecscript.d/openvswitch.vnet.sh
@@ -13,15 +13,15 @@ chroot $1 $SHELL -ex <<'EOS'
   releasever=$(< /etc/yum/vars/releasever)
   majorver=${releasever%%.*}
 
-  openvswitch_version=2.3.1-1
+  openvswitch_version=2.3.1
 
   repourl=http://dlc.openvnet.axsh.jp/packages/rhel/6/third_party/current/x86_64
 
   case "${releasever}" in
     *)
-      curl -Lf -o openvswitch-kmod.rpm ${repourl}/kmod-openvswitch-${openvswitch_version}.el6.x86_64.rpm
+      curl -Lf -o openvswitch-kmod.rpm https://www.dropbox.com/s/m0ign1weh6axd5f/kmod-openvswitch-2.3.1-1.el6.x86_64.rpm?dl=0
       yum -y localinstall openvswitch-kmod.rpm
-      curl -Lf -o openvswitch.rpm ${repourl}/openvswitch-${openvswitch_version}.x86_64.rpm
+      curl -Lf -o openvswitch.rpm https://www.dropbox.com/s/dri05zf7diumg5o/openvswitch-2.3.1-1.x86_64.rpm?dl=0
       yum -y localinstall openvswitch.rpm
       ;;
   esac
